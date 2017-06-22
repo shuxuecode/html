c0f2ae20b0ff7ea26cff64186c57719911696fb3
zhaoshuxue <zhaoshuxue@163.com>
2017-06-22 23:46:41 +0800

    commitBy2017年06月22号23时46分40秒

--- upload.py
+++ upload.py
@@ -8,7 +8,7 @@ os.system("git pull")
 
 os.system("git add .")
 
-os.system("git commit -m commitByZsx")
+os.system("git commit -m commitBy%date:~0,4%年%date:~5,2%月%date:~8,2%号%time:~0,2%时%time:~3,2%分%time:~6,2%秒")
 
 os.system("git push")
 
