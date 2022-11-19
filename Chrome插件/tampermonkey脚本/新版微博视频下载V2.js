d7f262ccf015a2e3d93fe723b466008ef876a449
zhaoshuxue <zhaoshuxue@163.com>
2022-11-19 15:36:35 +0800

    commitBy2022-11-19-15:36:34

--- Chrome插件/tampermonkey脚本/新版微博视频下载V2.js
+++ Chrome插件/tampermonkey脚本/新版微博视频下载V2.js
@@ -34,14 +34,14 @@
     //     timeout: 2000
     // })
 
-    var initProgressDiv = function(){
+    var initProgressDiv = function () {
         // 尝试获取进度条
         let $progress = $("body").find('progress')
 
         // 进度条不存在时，生成一个
         if ($progress.length === 0) {
             var div = '<div style=" height: 50px; width: 200px; border: 2px solid #ddd; z-index: 9999999999999; position: fixed; top: 10px; right: 10px; background-color: antiquewhite;">'
-            
+
             div += '<div class="notification" style="">123</div>'
             div += '<progress max="1" style="margin-left:10px;" />'
 
@@ -49,14 +49,14 @@
 
             $("body").prepend(div)
         }
-        
+
         $progress = $("body").find('progress')
         $progress[0].value = 0;
-        
+
     }
 
 
-    var handleFeedBody = function(_this){
+    var handleFeedBody = function (_this) {
         initProgressDiv();
 
         // console.warn(_this)
@@ -68,9 +68,9 @@
         var feedBody = btn.parents("footer").prev()
         // console.info(feedBody)
         var videoDiv = feedBody.find(".wbp-video")
-        if(videoDiv){
+        if (videoDiv) {
             var v = videoDiv.find("video")
-            if(v && v.length){
+            if (v && v.length) {
                 $("body").find('.notification').html("解析视频成功")
                 var src = v.attr("src")
                 console.log(src)
@@ -83,7 +83,7 @@
                     var width = btn.parents('footer').width()
                     var top = btn.parents('footer').offset().top
                     var left = btn.parents('footer').offset().left + width
-                    var dd = $('<div style="position: absolute; top: '+top+'px; left: ' + left + 'px; width: 10em; height: 30px; z-index:999999;"><progress max="1"/></div>')
+                    var dd = $('<div style="position: absolute; top: ' + top + 'px; left: ' + left + 'px; width: 10em; height: 30px; z-index:999999;"><progress max="1"/></div>')
                     // var dd = $('<div style="position: relative; top: -30px; left: 10em; float: right; width: 10em; height: 30px;"><progress max="1"/></div>')
                     // btn.parents('footer').append(dd)
                     $("body").prepend(dd)
@@ -92,9 +92,9 @@
                 progress[0].value = 0;
 
                 // const a = $('<div>下载视频</div>');
-                
+
                 const a = $(`<button class="woo-like-main"><span class="woo-like-count">下载视频</span></button>`);
-                
+
                 var contentDiv = feedBody.find(".wbpro-feed-content")
                 console.warn(contentDiv[0].innerText.indexOf('\n'))
                 var txt = contentDiv[0].innerText
@@ -104,24 +104,24 @@
                 var mp4Name = txt + ".mp4"
                 console.log('contentDiv = ', contentDiv)
 
-                a.click(function(){
+                a.click(function () {
                     GM_download({
                         url: src,
                         name: mp4Name, //"视频.mp4",
-                        onprogress: function(p) {
-            
+                        onprogress: function (p) {
+
                             const value = p.loaded / p.total;
                             progress[0].value = value;
                             $("body").find('progress')[0].value = value
                             console.warn(value)
                             if (value == 1) {
-                                
+
                             }
                         },
-                        onerror: function(e) {
-            
+                        onerror: function (e) {
+
                             console.error(e);
-            
+
                             // Tip.error("视频下载出错！");
                         }
                     });
@@ -136,52 +136,97 @@
                 // GM_setClipboard(src)
             }
         }
-        
+
     }
 
 
+    var videoResolver = function ($footer) {
+        // 获取主题内容
+        var feedBody = $footer.prev()
+        var videoDiv = feedBody.find(".wbp-video")
+        // 获取视频元素
+        if (videoDiv && videoDiv.length) {
+            var v = videoDiv.find("video")
+            if (v && v.length) {
+                // 解析视频地址
+                var src = v.attr("src")
+                src = "http:" + src
+
+                // 生成下载按钮
+                const downloadBtn = $(`<button class="woo-like-main"><span class="woo-like-count">下载视频</span></button>`);
+                downloadBtn.click(function () {
+                    GM_download({
+                        url: src,
+                        // name: mp4Name, //"视频.mp4",
+                        name: "视频.mp4",
+                        onprogress: function (p) {
+                            const value = p.loaded / p.total;
+                            // progress[0].value = value;
+                            // $("body").find('progress')[0].value = value
+                            console.warn(value)
+                            if (value == 1) {
+
+                            }
+                        },
+                        onerror: function (e) {
+                            console.error(e);
+                            // Tip.error("视频下载出错！");
+                        }
+                    });
+                });
+
+                var itemDiv = $(`<div class="woo-box-item-flex"></div>`)
+                itemDiv.append(downloadBtn)
+
+                $footer.children().append(itemDiv)
+            } else {
+                const $btn = $('<div class="woo-box-item-flex"><button class="woo-like-main"><span class="woo-like-count">不是视频2</span></button></div>')
+                $footer.children().append($btn)
+            }
+        } else {
+            console.log('videoDiv ', feedBody)
+            const $btn = $('<div class="woo-box-item-flex"><button class="woo-like-main"><span class="woo-like-count">不是视频1</span></button></div>')
+            $footer.children().append($btn)
+        }
+    }
 
 
     // 初始化按钮
-    var initBtn = function () {
+    var initVideoBtn = function () {
         // 获取所有的评论区
         var $likeBtn = $(".woo-like-count,.woo-like-liked")
-        // window.zhao = $likeBtn
         for (var i = 0; i < $likeBtn.length; i++) {
-        // for (var i = 0; i < 1; i++) {
             var $span = $($likeBtn[i])
             var boxFlex = $span.parents("footer").children()
-            console.log(boxFlex)
             if (boxFlex.children().length > 3) {
-                // 如果已经初始化则跳过
+                // 如果大于3则表示已经初始化过了，跳过
                 continue;
             }
 
-            const $btn = $('<button class="woo-like-main"><span class="woo-like-count">解析</span></button>')
-            $btn.click(handleFeedBody);
+            videoResolver($span.parents("footer"))
 
-            const $li = $(`<div class="woo-box-item-flex">`
-                // + `<button class="woo-like-main"><span class="woo-like-count">解析</span></button>`
-                // + `<button class="woo-like-main"><span class="woo-like-count">解析2</span></button>`
-                // + `<button class="woo-like-main"><span class="woo-like-count">解析3</span></button>`
-                + `</div>`);
+            // const $btn = $('<button class="woo-like-main"><span class="woo-like-count">解析</span></button>')
+            // $btn.click(handleFeedBody);
 
-            $li.append($btn)
+            // const $li = $(`<div class="woo-box-item-flex">`
+            //     // + `<button class="woo-like-main"><span class="woo-like-count">解析</span></button>`
+            //     // + `<button class="woo-like-main"><span class="woo-like-count">解析2</span></button>`
+            //     // + `<button class="woo-like-main"><span class="woo-like-count">解析3</span></button>`
+            //     + `</div>`);
 
-            boxFlex.append($li);
+            // $li.append($btn)
+
+            // boxFlex.append($li);
 
             // $ul.append('<li><a href="javascript:void(0);" class="S_txt2" onclick="handleWBFeedDetail(this)">点击</a></li>')
         }
-        // $(".WB_row_r4 li").css("width", "25%")
     }
-
-
 
 
 
     // setInterval(btn, 5000);
-    setTimeout(initBtn, 2000);
-    setTimeout(initBtn, 5000);
-    setTimeout(initBtn, 10000);
-    setTimeout(initBtn, 15000);
+    setTimeout(initVideoBtn, 2000);
+    setTimeout(initVideoBtn, 5000);
+    setTimeout(initVideoBtn, 10000);
+    setTimeout(initVideoBtn, 15000);
 })();
