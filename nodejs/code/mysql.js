b1289680c76f1185eb7e446f32038d59f5692599
zhaoshuxue <zhaoshuxue@163.com>
2023-07-19 21:25:59 +0800

    commitBy2023-07-19-21:25:59

--- nodejs/code/mysql.js
+++ nodejs/code/mysql.js
@@ -13,3 +13,29 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   console.log('The solution is: ', results[0].solution);
 });
 
+connection.query('SELECT * from x_user', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+    console.log('The solution is: ', results[0]);
+    console.log('The solution is: ', results[0].id);
+    console.log('The solution is: ', results[0].username);
+});
+
+// connection.end();
+
+// 插入数据
+
+var  addSql = 'INSERT INTO x_user(id,username,password,create_time,update_time) VALUES(?,?,?,now(),now())';
+var  addSqlParams = [11, '测试', '123456'];
+
+connection.query(addSql, addSqlParams, function(err, result) {
+    if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+    }
+    console.log('INSERT ID:',result);    
+});
+
+connection.end();
+
+
