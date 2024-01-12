bae540d9e41e195712f18f6d76b549d04900cfe2
zhaoshuxue <zhaoshuxue@163.com>
2024-01-12 23:41:42 +0800

    commitBy2024-01-12-23:41:41

--- Chrome插件/noteByGit/js/main.js
+++ Chrome插件/noteByGit/js/main.js
@@ -3,8 +3,29 @@
 
 function test() {
     alert(123)
+
+    localStorage.getItem
 }
+
+function setToken() {
+    var token = document.getElementById('token').value;
+    localStorage.setItem('my_github_token', token);
+}
+
+
+function getToken() {
+    var token = localStorage.getItem('my_github_token');  
+    document.getElementById('token').value = token;
+}
+
 
 
 let btn = document.getElementById('btn')
 btn.onclick = test;
+
+document.getElementById('updateToken').onclick = setToken;
+document.getElementById('showToken').onclick = getToken;
+
+
+
+
